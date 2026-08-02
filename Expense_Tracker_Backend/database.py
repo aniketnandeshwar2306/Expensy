969f13dd8d296532e1f2fdb3e9f8656100f7c843
Aniket <aniketnandeshwar2306@gmail.com>
2026-08-02 21:49:10 +0530

    add certifi for MongoDB Atlas SSL handshake fix

--- Expense_Tracker_Backend/database.py
+++ Expense_Tracker_Backend/database.py
@@ -1,9 +1,14 @@
 import os
 from pymongo import MongoClient  # type: ignore
+import certifi
 
 MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
 
-client = MongoClient(MONGODB_URL)
+# Use certifi for TLS CA certificates if connecting to remote MongoDB Atlas
+if "mongodb+srv" in MONGODB_URL:
+    client = MongoClient(MONGODB_URL, tlsCAFile=certifi.where())
+else:
+    client = MongoClient(MONGODB_URL)
 
 db = client["expense-tracker"]
 
